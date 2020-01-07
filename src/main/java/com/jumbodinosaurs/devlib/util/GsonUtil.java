5ab863ac024489c0e9e10e454612665f09fdb180
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-01-06 18:54:45 -0700

    Adjustments - added HolderList Functions
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/devlib/util/GsonUtil.java
+++ src/main/java/com/jumbodinosaurs/devlib/util/GsonUtil.java
@@ -5,12 +5,87 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 import com.google.typeadapters.gson.RuntimeTypeAdapterFactory;
+import com.jumbodinosaurs.devlib.util.objects.ObjectHolder;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class GsonUtil
 {
+    
+    public static <E> void saveObjectsToHolderList(File fileToSaveTo, ArrayList<E> objectsToSave, Class classType)
+    {
+        GsonBuilder builder = new GsonBuilder().setPrettyPrinting()
+                                               .registerTypeAdapterFactory(getClassPathRuntimeTypeAdapterFactory(
+                                                       classType));
+        Gson gson = builder.create();
+        ArrayList<ObjectHolder> objectHolders = new ArrayList<ObjectHolder>();
+        for(E object : objectsToSave)
+        {
+            objectHolders.add(new ObjectHolder(object.getClass(), gson.toJson(object)));
+        }
+        GeneralUtil.writeContents(fileToSaveTo, new Gson().toJson(objectHolders), false);
+    }
+    
+    public static <E> ArrayList<E> readObjectHoldersList(File fileToRead,
+                                                         Class classType,
+                                                         TypeToken<ArrayList<E>> typeToken)
+    {
+        /* When loading a list of polymorphic objects with Gson
+         * The problem that all the subtypes aren't in the class path can arise
+         * to get around this problem You can use an object holder and save the JSONized objects
+         * in the object holders data field and then serialize the object holders and then read
+         * the object holders back in and check whether the object they are holding is in the classpath
+         * this allows you to save data and easily read in objects using the RuntimeTypeAdapterFactory
+         * rather than throwing it all away
+         *
+         *  Process
+         *  Read in the file assuming it's Object Holders
+         *  Check each object holder to see if it's object is in the class path
+         *  Add it to a JSON String for RuntimeTypeAdapter Context if it in the class path
+         *  Load and return a list of the specified object from the context JSON String
+         *  */
+        try
+        {
+            //Read in the file assuming it's Object Holders
+            ArrayList<ObjectHolder> objectHolders = readList(fileToRead,
+                                                             ObjectHolder.class,
+                                                             new TypeToken<ArrayList<ObjectHolder>>() {},
+                                                             false);
+            
+            /* Check each object holder to see if it's object is in the class path
+             * Add it to a JSON String for RuntimeTypeAdapter Context if it in the class path
+             *
+             * To make it easy to read and return a list of the specified object
+             * we can create a JSON list from the Available subclasses
+             * Format: ["JSON OBJECTS SEPARATED BY COMMAS HERE"]
+             */
+            String classPathContext = "[";
+            for(ObjectHolder objectHolder : objectHolders)
+            {
+                if(objectHolder.isInClassPath())
+                {
+                    classPathContext += objectHolder.getData() + ",";
+                }
+            }
+            //Remove the last comma if any objects were added
+            boolean objectsWereAdded = classPathContext.length() > 1;
+            if(objectsWereAdded)
+            {
+                //Remove the last Comma
+                classPathContext = classPathContext.substring(0, classPathContext.length() - 1);
+            }
+            //Add the last Bracket for the list
+            classPathContext += "]";
+            
+            return readList(classPathContext, classType, typeToken, true);
+        }
+        catch(JsonParseException e)
+        {
+            throw e;
+        }
+    }
+    
     
     public static <E> ArrayList<E> readList(File fileToRead,
                                             Class classType,
