5799c573de8cc8db6f8e93f0268a505ae53756f1
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-02-03 15:18:29 -0700

    Adjustments - added listing resource capabilities
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/devlib/util/ResourceLoaderUtil.java
+++ src/main/java/com/jumbodinosaurs/devlib/util/ResourceLoaderUtil.java
@@ -3,12 +3,26 @@ package com.jumbodinosaurs.devlib.util;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 public class ResourceLoaderUtil
 {
     public ClassLoader getLoader()
     {
         return getClass().getClassLoader();
+    }
+    
+    public List<File> getFiles(String resourceDir)
+    {
+        File resourceDirFile = getResource(resourceDir);
+        if(resourceDirFile == null)
+        {
+            return null;
+        }
+        List<File> resources = Arrays.asList(resourceDirFile.listFiles());
+        return resources;
+        
     }
     
     public File getResource(String resourcePath)
