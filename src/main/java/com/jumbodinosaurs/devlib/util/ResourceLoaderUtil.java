ad2eaf7a68530569e221f5aa9a23906c2ee069e2
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-02-05 21:40:59 -0700

    Adjustments - made changes to Resource Loader (testing undone)
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

--- src/main/java/com/jumbodinosaurs/devlib/util/ResourceLoaderUtil.java
+++ src/main/java/com/jumbodinosaurs/devlib/util/ResourceLoaderUtil.java
@@ -1,10 +1,14 @@
 package com.jumbodinosaurs.devlib.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ResourceLoaderUtil
 {
@@ -15,14 +19,55 @@ public class ResourceLoaderUtil
     
     public List<File> getFiles(String resourceDir)
     {
-        File resourceDirFile = getResource(resourceDir);
-        if(resourceDirFile == null)
+        ArrayList<File> files = new ArrayList<File>();
+        try
+        {
+            for(String fileName : listFiles(resourceDir))
+            {
+                System.out.println(fileName);
+                files.add(getResource(fileName));
+            }
+            return files;
+        }
+        catch(IOException e)
         {
             return null;
         }
-        List<File> resources = Arrays.asList(resourceDirFile.listFiles());
-        return resources;
+    }
+    
+    //https://stackoverflow.com/questions/11012819/how-can-i-get-a-resource-folder-from-inside-my-jar-file
+    public ArrayList<String> listFiles(String resourceDir) throws IOException
+    {
+        ArrayList<String> fileNames = new ArrayList<String>();
+        File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        if(jarFile.isFile())
+        {  // Run with JAR file
+            JarFile jar = new JarFile(jarFile);
+            Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+            while(entries.hasMoreElements())
+            {
+                String name = entries.nextElement().getName();
+                if(name.startsWith(resourceDir + "/"))
+                { //filter according to the path
+                    fileNames.add(resourceDir + "/" + name);
+                }
+            }
+            jar.close();
+        }
+        else
+        { // Run with IDE
+            
+            for(File file : getResource(resourceDir).listFiles())
+            {
+                fileNames.add(resourceDir + "/" + file.getName());
+            }
+        }
         
+        for(String string : fileNames)
+        {
+            System.out.println("File Name: " + string);
+        }
+        return fileNames;
     }
     
     public File getResource(String resourcePath)
