0a9aa8a0f02773dd064dbd5cac58ed48797b6ba7
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-11-05 17:06:49 -0600

    Adjustments - added logback and ConsoleLogger
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

---
+++ src/main/java/com/jumbodinosaurs/devlib/log/LogManager.java
@@ -0,0 +1,10 @@
+package com.jumbodinosaurs.devlib.log;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogManager
+{
+    public static Logger consoleLogger = LoggerFactory.getLogger("ConsoleLogger");
+}
